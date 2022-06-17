7e7f088c74c18448b9737c4f03a690b61bfee1a3
ali sartipi <105682168+alistp27@users.noreply.github.com>
2022-06-17 19:56:18 +0430

    Navbar

---
+++ 5/app.js
@@ -0,0 +1,7 @@
+let links = document.getElementById('info');
+let button = document.getElementById('navbtn');
+
+button.addEventListener('click',()=>{
+    links.classList.toggle('show');
+    console.log("gi")
+})
